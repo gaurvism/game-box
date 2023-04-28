55111a4bc8dceb5ffb89557c8baab1291b57c4c0
Gaurav Sharma <37509474+gaurvism@users.noreply.github.com>
2023-04-28 14:49:26 +0530

    Refactored - fetch games with react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'c1a678d730a747078a4871f5e537114b'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "c1a678d730a747078a4871f5e537114b",
+  },
+});
